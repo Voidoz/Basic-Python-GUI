03cb9b9eb6f93abc81fd76465fc58bab2fbf1d40
Voidoz <41652531+Voidoz@users.noreply.github.com>
2019-05-01 10:57:19 +1200

    changed row variable name

--- simple_gui.py
+++ simple_gui.py
@@ -9,13 +9,13 @@ root.geometry("1080x720")
 
 
 # variables
-hiRow = 3
+rowNum = 3
 
 
 # definitions
 def speak():
-    Label(root, text="Hi",).grid(row=hiRow, column=0, sticky=W),
-    hiRow + 1
+    Label(root, text="Hi",).grid(row=rowNum, column=0, sticky=W),
+    rowNum + 1
 def addButton():
     Button(root, text="Say 'Hi'", width=14, command=speak).grid(row=2, column=0, sticky=W)
 
