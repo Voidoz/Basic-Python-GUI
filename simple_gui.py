a871a4960e21132aed7b9adfe0db01cbbfee93ac
Voidoz <41652531+Voidoz@users.noreply.github.com>
2019-04-30 21:41:14 +1200

    fixed code so it works

--- simple_gui.py
+++ simple_gui.py
@@ -1,26 +1,25 @@
-# import gui library
+# import GUI library
 from tkinter import *
 
-gui = Tk()
-gui.title("Simple GUI")
-gui.geometry("1080x720")
-hiRow = 3
+
+# GUI config
+root = Tk()
+root.title("Simple GUI")
+root.geometry("1080x720")
 
 
-def addHiRow():
-    hiRow += 1
+# definitions
 def speak():
-    Label(gui, text="Hi",).grid(row=hiRow, column=0, sticky=W),
-    addHiRow()
+    Label(root, text="Hi",).grid(row=hiRow, column=0, sticky=W),
 def addButton():
-    Button(gui, text="Say 'Hi'", width=14, command=speak()).grid(row=2, column=0, sticky=W)
+    Button(root, text="Say 'Hi'", width=14, command=speak).grid(row=2, column=0, sticky=W)
 
 
-# add a label
-Label(gui, text="This is my GUI").grid(row=0, column=0, sticky=W)
+# content
+Label(root, text="This is my GUI").grid(row=0, column=0, sticky=W)
 
-# add a button
-Button(gui, text="Add Button", width=9, command=addButton).grid(row=1, column=0, sticky=W)
+Button(root, text="Add Button", width=9, command=addButton).grid(row=1, column=0, sticky=W)
 
-# start the gui
-gui.mainloop()
+
+# start the GUI
+root.mainloop()
